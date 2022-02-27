6f623bc62a93af2f75498714c047df0a41fed30e
Alexandre Klein <dalfgan77@gmail.com>
2022-02-27 23:17:42 +0900

    improve code

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,6 @@
 'use strict';
 const axios = require('axios');
-const { LCDClient, Coin } = require('@terra-money/terra.js');
+const { LCDClient } = require('@terra-money/terra.js');
 
 // How to get these values?
 // Go to https://prismprotocol.app/swap and "inspect" the code and look at the request sent
@@ -9,7 +9,7 @@ const XPRISM_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','oper
 const YLUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'cw20':'terra17wkadg0tah554r35x6wvff0y5s7ve8npcjfuhz'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
 const PLUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'cw20':'terra1tlgelulz9pdkhls6uglfn5lmxarx7f2gxtdzh2'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
 const CLUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'cw20':'terra13zaagrrrxj47qjwczsczujlvnnntde7fdt0mau'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
-const LUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'native':'uluna'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
+// const LUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'native':'uluna'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
 
 const dev = {
   url: 'https://bombay-lcd.terra.dev',
@@ -25,6 +25,8 @@ const prod = {
   contractOther: 'terra1yrc0zpwhuqezfnhdgvvh7vs5svqtgyl7pu3n6c'
 };
 
+// the price receives from the API has no decimal
+const DECIMAL = 1e6;
 
 module.exports.run = async (event, context) => {
   const time = new Date();
@@ -37,50 +39,34 @@ module.exports.run = async (event, context) => {
     chainID: config.chainId,
   });
  
-  // const marketParams = await terra.market.parameters();
-  // console.log(marketParams.base_pool);
-
-  // const exchangeRates = await terra.oracle.exchangeRates();
-  // console.log(`1 LUNA = ${exchangeRates.get('uusd').amount}`);
-
+  // LUNA
   const exchangeRate = await terra.oracle.exchangeRate('uusd');
   const lunaValue = exchangeRate.amount;
   console.log(`LUNA:  ${lunaValue}`);
 
-  
-  const prismResponse = await terra.wasm.contractQuery(
-    config.contractPrism,
-    PRISM_QUERY,
-  );
-  const prismValue = parseFloat(prismResponse.return_amount) / Math.pow(10, prismResponse.return_amount.length);
+  // PRISM
+  const prismResponse = await terra.wasm.contractQuery(config.contractPrism, PRISM_QUERY);
+  const prismValue = prismResponse.return_amount/DECIMAL;
   console.log(`PRISM:  ${prismValue}`);
 
-  const xprismResponse = await terra.wasm.contractQuery(
-    config.contractOther,
-    XPRISM_QUERY,
-  );
-  const xprismValue = parseFloat(xprismResponse.amount) / Math.pow(10, xprismResponse.amount.length);
+  // xPRISM
+  const xprismResponse = await terra.wasm.contractQuery(config.contractOther, XPRISM_QUERY);
+  const xprismValue = xprismResponse.amount/DECIMAL;
   console.log(`xPRISM: ${xprismValue}`);
 
-  const ylunaResponse = await terra.wasm.contractQuery(
-    config.contractOther,
-    YLUNA_QUERY,
-  );
-  const ylunaValue = parseFloat(ylunaResponse.amount) / Math.pow(10, ylunaResponse.amount.length - 2);
+  // yLUNA
+  const ylunaResponse = await terra.wasm.contractQuery(config.contractOther, YLUNA_QUERY);
+  const ylunaValue = ylunaResponse.amount/DECIMAL;
   console.log(`yLUNA: ${ylunaValue}`);
 
-  const plunaResponse = await terra.wasm.contractQuery(
-    config.contractOther,
-    PLUNA_QUERY,
-  );
-  const plunaValue = parseFloat(plunaResponse.amount) / Math.pow(10, plunaResponse.amount.length - 2);
+  // pLUNA
+  const plunaResponse = await terra.wasm.contractQuery(config.contractOther, PLUNA_QUERY);
+  const plunaValue = plunaResponse.amount/DECIMAL;
   console.log(`pLUNA: ${plunaValue}`);
 
-  const clunaResponse = await terra.wasm.contractQuery(
-    config.contractOther,
-    CLUNA_QUERY,
-  );
-  const clunaValue = parseFloat(clunaResponse.amount) / Math.pow(10, clunaResponse.amount.length - 2);
+  // cLUNA
+  const clunaResponse = await terra.wasm.contractQuery(config.contractOther, CLUNA_QUERY);
+  const clunaValue = clunaResponse.amount/DECIMAL;
   console.log(`cLUNA: ${clunaValue}`);
 
 
