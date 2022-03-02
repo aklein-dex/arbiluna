f1fdbf015426d388098ac924c504f3654b684bcd
Alexandre Klein <dalfgan77@gmail.com>
2022-03-02 23:38:39 +0900

    improve code

--- src/handler.js
+++ src/handler.js
@@ -1,15 +1,5 @@
 'use strict';
-// const axios = require('axios');
 const { LCDClient } = require('@terra-money/terra.js');
-
-// How to get these values?
-// Go to https://prismprotocol.app/swap and "inspect" the code and look at the request sent
-const PRISM_QUERY = {'simulation':{'offer_asset':{'amount':'1000000','info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}}};
-const XPRISM_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'cw20':'terra1042wzrwg2uk6jqxjm34ysqquyr9esdgm5qyswz'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
-const YLUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'cw20':'terra17wkadg0tah554r35x6wvff0y5s7ve8npcjfuhz'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
-const PLUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'cw20':'terra1tlgelulz9pdkhls6uglfn5lmxarx7f2gxtdzh2'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
-const CLUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'cw20':'terra13zaagrrrxj47qjwczsczujlvnnntde7fdt0mau'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
-// const LUNA_QUERY = {'simulate_swap_operations':{'offer_amount':'1000000','operations':[{'prism_swap':{'offer_asset_info':{'native':'uluna'},'ask_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'}}},{'prism_swap':{'offer_asset_info':{'cw20':'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw'},'ask_asset_info':{'native':'uusd'}}}]}};
 
 const dev = {
   url: 'https://bombay-lcd.terra.dev',
@@ -22,11 +12,55 @@ const prod = {
   url: 'https://lcd.terra.dev',
   chainId: 'columbus-5',
   contractPrism: 'terra19d2alknajcngdezrdhq40h6362k92kz23sz62u',
-  contractOther: 'terra1yrc0zpwhuqezfnhdgvvh7vs5svqtgyl7pu3n6c'
+  contractOther: 'terra1yrc0zpwhuqezfnhdgvvh7vs5svqtgyl7pu3n6c',
+  cw20Prism: 'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw',
+  cw20Xprism: 'terra1042wzrwg2uk6jqxjm34ysqquyr9esdgm5qyswz',
 };
+
+// the addresses of tokens
+const CW20 = new Map();
+CW20.set('prism', 'terra1dh9478k2qvqhqeajhn75a2a7dsnf74y5ukregw');
+CW20.set('xprism', 'terra1042wzrwg2uk6jqxjm34ysqquyr9esdgm5qyswz');
+CW20.set('yluna', 'terra17wkadg0tah554r35x6wvff0y5s7ve8npcjfuhz');
+CW20.set('pluna', 'terra1tlgelulz9pdkhls6uglfn5lmxarx7f2gxtdzh2');
+CW20.set('cluna', 'terra13zaagrrrxj47qjwczsczujlvnnntde7fdt0mau');
 
 // the price receives from the API has no decimal
 const DECIMAL = 1e6;
+
+// this is 1$
+const ONE_DOLLAR = 1000000;
+
+function getOperation(tokenOffer, tokenAsk) {
+  const ask = tokenAsk === 'UST' ? { native: 'uusd' } : { cw20: CW20.get(tokenAsk) };
+  return { prism_swap: { offer_asset_info: { cw20: CW20.get(tokenOffer) }, ask_asset_info: ask }};
+}
+
+function swapUstToPrism(amount) {
+  return { simulation: { offer_asset: { amount: `${amount}`, info: { cw20: CW20.get('prism') }}}};
+}
+
+function swapUstToXprism(amount) {
+  return { simulate_swap_operations: { offer_amount: `${amount}`, operations: [ getOperation('xprism', 'prism'), getOperation('prism', 'UST')] }};
+}
+
+function swapUstToYluna(amount) {
+  return { simulate_swap_operations: { offer_amount: `${amount}`, operations: [ getOperation('yluna', 'prism'), getOperation('prism', 'UST')] }};
+}
+
+function swapUstToPluna(amount) {
+  return { simulate_swap_operations: { offer_amount: `${amount}`, operations: [ getOperation('pluna', 'prism'), getOperation('prism', 'UST')] }};
+}
+
+function swapUstToCluna(amount) {
+  return { simulate_swap_operations: { offer_amount: `${amount}`, operations: [ getOperation('cluna', 'prism'), getOperation('prism', 'UST')] }};
+}
+
+
+// find the % difference
+function getArbitrage(tokenA, tokenB) {
+  return ((tokenB - tokenA) * 100 / tokenA).toFixed(2);
+}
 
 module.exports.run = async (event, context) => {
   const time = new Date();
@@ -41,12 +75,14 @@ module.exports.run = async (event, context) => {
  
 
   // PRISM
-  const prismResponse = await terra.wasm.contractQuery(config.contractPrism, PRISM_QUERY);
+  // const prismResponse = await terra.wasm.contractQuery(config.contractPrism, PRISM_QUERY);
+  const prismResponse = await terra.wasm.contractQuery(config.contractPrism, swapUstToPrism(ONE_DOLLAR));
   const prismValue = prismResponse.return_amount/DECIMAL;
   console.log(`PRISM:  ${prismValue}`);
 
   // xPRISM
-  const xprismResponse = await terra.wasm.contractQuery(config.contractOther, XPRISM_QUERY);
+  // const xprismResponse = await terra.wasm.contractQuery(config.contractOther, XPRISM_QUERY);
+  const xprismResponse = await terra.wasm.contractQuery(config.contractOther, swapUstToXprism(ONE_DOLLAR));
   const xprismValue = xprismResponse.amount/DECIMAL;
   console.log(`xPRISM: ${xprismValue}`);
 
@@ -56,43 +92,48 @@ module.exports.run = async (event, context) => {
   console.log(`LUNA:  ${lunaValue}`);
 
   // yLUNA
-  const ylunaResponse = await terra.wasm.contractQuery(config.contractOther, YLUNA_QUERY);
+  const ylunaResponse = await terra.wasm.contractQuery(config.contractOther, swapUstToYluna(ONE_DOLLAR));
   const ylunaValue = ylunaResponse.amount/DECIMAL;
   console.log(`yLUNA: ${ylunaValue}`);
 
   // pLUNA
-  const plunaResponse = await terra.wasm.contractQuery(config.contractOther, PLUNA_QUERY);
+  const plunaResponse = await terra.wasm.contractQuery(config.contractOther, swapUstToPluna(ONE_DOLLAR));
   const plunaValue = plunaResponse.amount/DECIMAL;
   console.log(`pLUNA: ${plunaValue}`);
 
   // cLUNA
-  const clunaResponse = await terra.wasm.contractQuery(config.contractOther, CLUNA_QUERY);
+  const clunaResponse = await terra.wasm.contractQuery(config.contractOther, swapUstToCluna(ONE_DOLLAR));
   const clunaValue = clunaResponse.amount/DECIMAL;
   console.log(`cLUNA: ${clunaValue}`);
 
 
-  console.log('\nArbitrage opportunity:')
-  if (prismValue > xprismValue) {
-    console.log('  change your PRISM for XPRISM');
+  console.log('\nArbitrage opportunity:');
+
+  const prismXprismArb = getArbitrage(prismValue, xprismValue);
+  if (prismXprismArb < 0) {
+    console.log(`  Arb = ${prismXprismArb * -1 }% xPRISM -> PRISM`);
   } else {
-    console.log('  change your xPRISM for PRISM');
+    console.log(`  Arb = ${prismXprismArb}% PRISM -> xPRISM`);
   }
 
-  if (lunaValue > clunaValue) {
-    console.log('  change your LUNA for cLUNA');
+  const lunaClunaArb = getArbitrage(lunaValue, clunaValue);
+  if (lunaClunaArb < 0) {
+    console.log(`  Arb = ${lunaClunaArb * -1 }% cLUNA -> LUNA`);
   } else {
-    console.log('  change your cLUNA for LUNA');
+    console.log(`  Arb = ${lunaClunaArb}% LUNA -> cLUNA`);
   }
 
-  if (lunaValue > (ylunaValue + plunaValue)) {
-    console.log('  change your LUNA for pLUNA and yLuna');
+  const lunaPYlunaArb = getArbitrage(lunaValue, (plunaValue + ylunaValue));
+  if (lunaPYlunaArb < 0) {
+    console.log(`  Arb = ${lunaPYlunaArb * -1 }% pLUNA + yLUNA -> LUNA`);
   } else {
-    console.log('  change your pLUNA and yLuna for LUNA');
+    console.log(`  Arb = ${lunaPYlunaArb}% LUNA -> pLUNA + yLUNA`);
   }
 
-  if (clunaValue > (ylunaValue + plunaValue)) {
-    console.log('  change your cLUNA for pLUNA and yLuna');
+  const clunaPYlunaArb = getArbitrage(clunaValue, (plunaValue + ylunaValue));
+  if (clunaPYlunaArb < 0) {
+    console.log(`  Arb = ${clunaPYlunaArb * -1 }% pLUNA + yLUNA -> cLUNA`);
   } else {
-    console.log('  change your pLUNA and yLuna for cLUNA');
+    console.log(`  Arb = ${clunaPYlunaArb}% cLUNA -> pLUNA + yLUNA`);
   }
 };
